5eb2e465d01897e25af74023fab6d2372cf97193
Jibreel Powell <jibreel.powell@gmail.com>
2022-01-27 17:24:03 -0600

    create Snapshot class

---
+++ app/src/main/java/com/jibreelpowell/theundomachine/snapshot/Snapshot.kt
@@ -0,0 +1,7 @@
+package com.jibreelpowell.theundomachine.snapshot
+
+data class Snapshot(
+    val timestamp: Long,
+    val text: String
+) {
+}
---
+++ app/src/main/java/com/jibreelpowell/theundomachine/viewmodels/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.jibreelpowell.theundomachine.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val currentText = MutableLiveData<String>()
+
+    val saveHistory =
+}
