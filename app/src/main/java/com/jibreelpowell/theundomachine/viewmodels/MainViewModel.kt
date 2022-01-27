ee0019658b0f112b99379c05bb1182bad59b848d
Jibreel Powell <jibreel.powell@gmail.com>
2022-01-27 17:25:07 -0600

    fix dangle

--- app/src/main/java/com/jibreelpowell/theundomachine/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/jibreelpowell/theundomachine/viewmodels/MainViewModel.kt
@@ -2,10 +2,11 @@ package com.jibreelpowell.theundomachine.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jibreelpowell.theundomachine.snapshot.Snapshot
 
 class MainViewModel : ViewModel() {
 
     val currentText = MutableLiveData<String>()
 
-    val saveHistory =
+    val saveHistory: MutableList<Snapshot> = arrayListOf()
 }
